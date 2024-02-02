476aac79a94ee266fd71444619a362f234fe61c0
David Ulrich <david.hau1@gmail.com>
2024-02-02 09:57:57 +0100

    added tiny_sort func

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 08:41:01 by dulrich           #+#    #+#             */
-/*   Updated: 2024/02/01 09:03:10 by dulrich          ###   ########.fr       */
+/*   Updated: 2024/02/02 09:57:33 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,4 +145,69 @@ int	stack_len(t_node *stack)
 		len++;
 	}
 	return (len);
+}
+
+void	swap(t_node **head)
+{
+	int	len;
+	
+	len = stack_len(*head);
+	if (*head == NULL || head == NULL || len == 1)
+		return ;
+	*head = (*head)->next;
+	(*head)->prev->prev = *head;
+	(*head)->prev->next = (*head)->next;
+	if ((*head)->next)
+		(*head)->next->prev = (*head)->prev;
+	(*head)->next = (*head)->prev;
+	(*head)->prev = NULL;
+}
+
+void	sa(t_node **a, bool checker)
+{
+	swap(a);
+	if (!checker)
+		write(1, "sa\n", 3);
+}
+
+void	sb(t_node **b, bool checker)
+{
+	swap(b);
+	if (!checker)
+		write(1, "sb\n", 3);
+}
+
+void	ss(t_node **a, t_node **b, bool checker)
+{
+	swap(a);
+	swap(b);
+	if (!checker)
+		write(1, "ss\n", 3);
+}
+
+t_node	*find_biggest(t_node *stack)
+{
+	t_node	*biggest_node;
+	int		biggest;
+
+	if (stack == NULL)
+		return (NULL);
+	biggest = INT_MIN;
+	while (stack)
+	{
+		if (stack->value > biggest)
+		{
+			biggest = stack->value;
+			biggest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (biggest_node);
+}
+
+void	tiny_sort(t_node **a)
+{
+	t_node	biggest_nbr;
+	biggest_nbr = find_biggest(*a);
+	if ()
 }
